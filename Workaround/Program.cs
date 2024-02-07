c6b2b096db6bdcc035b6129013a53b9ef7a396a4
bymadmr <mab.d@hotmail.com>
2024-02-08 00:25:44 +0300

    Add project files.

---
+++ Workaround/Program.cs
@@ -0,0 +1,20 @@
+﻿using Business.Concrete;
+using Entities.Concrete;
+
+static int Topla(int sayi1, int sayi2)
+{
+    int result = sayi1 + sayi2;
+    Console.WriteLine("Toplam: " + result);
+    return result;
+    
+}
+Person person = new Person();
+person.NationalIdentity = 123;
+person.FirstName = "Muhammed";
+person.LastName = "Demir";
+person.DateOfBirthYear = 1994;
+
+
+PttManager pttManager=new PttManager(new PersonManager());
+pttManager.GiveMask(person);
+Console.ReadLine();
